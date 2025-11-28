9ef96c913dfdf2ad1b43a93b308ea51f4f59047e
Caroline Pellet <ventvole@gmail.com>
2025-11-28 17:57:51 +0100

    skeletro for the page, uncomment the notification list, add semantic html

--- src/routes/app/(index)/+page.svelte
+++ src/routes/app/(index)/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
   import { Main } from '$components/main'
+  import UserNotificationsList from '$components/user-notifications/UserNotificationsList.svelte'
+  import UserNotificationsProvider from '$components/user-notifications/UserNotificationsProvider.svelte'
   import { notificationsStore } from '$stores/user-notifications'
   import { onMount } from 'svelte'
   import { _ } from 'svelte-i18n'
@@ -12,9 +14,13 @@
     {$_('Hello User', { values: { user: 'John Doe' } })}
   </h1>
 
-  <div class="md:h-[calc(100%-theme(spacing.navigation))]">
-    <!-- <UserNotificationsProvider let:activity let:initialized>
-      <div
+  <div class="flex flex-col gap-6 md:h-[calc(100%-theme(spacing.navigation))] md:flex-row">
+    <section class="flex-1">
+      <!-- KPI cards -->
+    </section>
+
+    <UserNotificationsProvider let:activity let:initialized>
+      <aside
         class="flex flex-col max-md:mt-6 {activity.length > 5
           ? 'md:h-full'
           : 'md:h-fit'} md:w-1/2 md:rounded-lg md:border md:bg-card md:px-4 md:py-1 lg:w-2/6">
@@ -25,7 +31,7 @@
             <div>{$_('Please Wait')}</div>
           </div>
         {/if}
-      </div>
-    </UserNotificationsProvider> -->
+        </aside>
+    </UserNotificationsProvider>
   </div>
 </Main.Basic>
